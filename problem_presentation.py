1bd3c654ca34c738c7150e11681869aa6917d2b5
Chilly-12 <141146455+Chilly-12@users.noreply.github.com>
2024-04-06 17:46:02 +0200

    problem_presentation.py

--- problem_presentation.py
+++ problem_presentation.py
@@ -10,24 +10,21 @@ def calculate_total_distance(adjacency_matrix, route):
     total_distance = 0
     n = len(route)
     
-    # Calculate the distance from the starting point to the first place
     start = route[0]
     current = route[1]
     total_distance += adjacency_matrix[start][current]
     
-    # Calculate the distances between consecutive places in the route
     for i in range(1, n):
         previous = route[i - 1]
         current = route[i]
         total_distance += adjacency_matrix[previous][current]
     
-    # Add the distance from the last place back to the starting point
     last = route[-1]
     total_distance += adjacency_matrix[last][start]
     
     return total_distance
 
-# Testing the function with a known solution
-sample_route = [0, 2, 3, 4, 1, 0]  # Dorado Park -> Khomasdal -> Katutura -> Eros -> Klein Windhoek -> Dorado Park
+
+sample_route = [0, 2, 3, 4, 1, 0]  
 total_distance = calculate_total_distance(adjacency_matrix, sample_route)
 print(f"Total distance for the route {sample_route}: {total_distance}")
